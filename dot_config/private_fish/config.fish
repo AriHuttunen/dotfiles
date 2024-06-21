e426a9cf38e242661b8aa1f87e2c081776eae68d
Ari <finnish.dancer@aris-mbp.kite-catfish.ts.net>
2024-06-21 21:28:34 +0300

    added fish shell

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+eval "$(/usr/local/bin/brew shellenv)"
+set -x VISUAL /usr/local/bin/hx
+set -x EDITOR /usr/local/bin/hx
+set -x PATH /Users/finnish.dancer/.juliaup/bin $PATH
