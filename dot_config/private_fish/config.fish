5d3e8d5bbbdc9fc6938a4667382565795cda398c
Ari <87063900+AriHuttunen@users.noreply.github.com>
2024-06-23 13:22:20 +0300

    merged home configurations

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,4 +6,10 @@ end
 eval "$(/usr/local/bin/brew shellenv)"
 set -x VISUAL /usr/local/bin/hx
 set -x EDITOR /usr/local/bin/hx
-set -x PATH /Users/finnish.dancer/.juliaup/bin $PATH
+set -x PATH ~/.juliaup/bin $PATH
+set -gx PATH ~/.cargo/bin $PATH
+set -gx PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $PATH
+
+function tailscale
+    /Applications/Tailscale.app/Contents/MacOS/Tailscale $argv
+end
